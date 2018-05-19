98cda0b3fde1b0c28a0c5d14736b5248a3f1d64c
PPrimon <pprimon@live.com>
2018-05-19 19:50:58 -0300

    Conexão com o banco
    Crud, adaptação de campos do menu, tipo e status.

--- portal/portal/admin/adm_conteudo.php
+++ portal/portal/admin/adm_conteudo.php
@@ -21,47 +21,66 @@
     <div class="card card-register mx-auto mt-5">
       <div class="card-header">Registro de Conteúdo</div>
       <div class="card-body">
-        <form method="post" action="conteudo.php">
+       <form method="post" action="teste_adm_conteudo.php" >
           <div class="form-group">
             <div class="form-row">
               <div class="col-md-6">
                 <label for="codigo">Código</label>
-                <input class="form-control" id="codigo" type="number" aria-describedby="codigoHelp" placeholder="Código da postagem">
+                <input class="form-control" name="codigo" type="number" aria-describedby="codigoHelp" placeholder="Código da postagem">
               </div>
               <div class="col-md-6">
-				<!-- acrescentar lista com tipos? quais tipos estarão disponiveis ?-->
                 <label for="tipo">Tipo</label>
-                <input class="form-control" id="tipo" type="text" aria-describedby="tipoHelp" placeholder="Entre com o tipo de postagem">
-              </div>
+				<select class="form-control"  name="tipo" type="text" aria-describedby="tipoHelp">
+					<option value="0"> Destaque </option>
+					<option value="1"> Conteudo </option>
+					<option value="2"> Noticia </option>
+					<option value="3"> DestaqueHotSite </option>
+				</select>	
+			 </div>
 			  <div class="col-md-6">
                 <label for="keyWord">Palavras Chave</label>
-                <input class="form-control" id="keyWord" type="text" aria-describedby="keyWordHelp" placeholder="Entre com palavras chave para pesquisa">
+                <input class="form-control" name="keywords" type="text" aria-describedby="keyWordHelp" placeholder="Entre com palavras chave para pesquisa">
               </div>
+			  <div class="col-md-6">
+                <label for="menuRelacionado">Menu Relacionado</label>
+                <select class="form-control" name="menuRelacionado" aria-describedby="menuRelacionado">
+					<option value="0"> Nenhum menu relacionado</option>
+					<?php
+							while ($registro = @mysql_fetch_array($result)) {
+
+								$codigo = $registro ["codigo"];
+								$nome = $registro ["nome"];
+								echo"<option value='$codigo'> $nome </option>";
+
+		}					
+					?>
+				</select>
+              </div>
+			 
             </div>
           </div>
 			<div class="form-group">
-				<label for="dtPublicacoa">Data de publicação</label>
-				<input class="form-control" id="data_publicado" type="date" aria-describedby="dataHelp">
-			</div>
-			<div class="form-group">
 				<label for="nome">Nome</label>
-				<input class="form-control" id="nome" type="text" aria-describedby="nomeHelp" placeholder="Entre com o nome">
+				<input class="form-control" name="nome" type="text" aria-describedby="nomeHelp" placeholder="Entre com o nome">
 			</div>
 			<div class="form-group">	
 				<label for="titulo">Titulo</label>
-				<input class="form-control" id="titulo" type="text" aria-describedby="tituloHelp" placeholder="Entre com o titulo">
+				<input class="form-control" name="titulo" type="text" aria-describedby="tituloHelp" placeholder="Entre com o titulo">
 			</div>
-			<div class="form-group">
-				<!-- acrescentar lista com status? quais status estarão disponiveis ?-->
-				<label for="status">Status</label>
-				<input class="form-control" id="status" type="text" aria-describedby="statusHelp" placeholder="Entre com o status">
+			 <div class="form-group">
+			<!-- colocar radio button ativo ou inativo -->
+				<label for="status"> Status </label>
+				<input class="form-control" name="status" type="radio" aria-describedby="statusHelp" value="1" /> Sim 
+				<input class="form-control" name="status" type="radio" aria-describedby="statusHelp" value="0" /> Não
+				
 			</div>
 			
 			<div class="form-group">
 				<label for="conteudo">Conteudo da publicação</label></br>
-				<textarea class="form-control" id="conteudo" placeholder="Entre com o conteudo da publicação" rows="6" cols="50"></textarea>
+				<textarea class="form-control" id="conteudo" name="conteudo" placeholder="Entre com o conteudo da publicação" rows="6" cols="50"></textarea>
 			
 			</div>
+			
 			<input type="submit" name="btn1" value="Inserir">
 			<input type="submit" name="btn2" value="Pesquisar">
 			<input type="submit" name="btn3" value="Alterar">
@@ -71,6 +90,7 @@
       </div>
     </div>
   </div>
+ 
   <?php
 		$con = new mysqli("baratheon0001.hospedagemdesites.ws","norto_fatecig","freiJoao59","norton_fatecig");
 		if(isset($_POST["btn1"]))
@@ -91,8 +111,10 @@
 		{
 		busca();
 		}
-		//$con->close();		
-	?>
+		//$con->close();
+
+		
+  ?>
   <!-- Bootstrap core JavaScript-->
   <script src="vendor/jquery/jquery.min.js"></script>
   <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -104,15 +126,25 @@
 
 <?php
 	
+	
+	function carregaMenu(){
+		
+		$con = new mysqli("baratheon0001.hospedagemdesites.ws","norto_fatecig","freiJoao59","norton_fatecig");
+		$sql = "select codigo, nome from menu where status=1";
+        $result = mysql_query($sql) or die ("Ops, deu Erro!!!");
+		$con->close();
+		
+
+   }
+	
 	function busca()
 	{
 			$con = new mysqli("baratheon0001.hospedagemdesites.ws","norto_fatecig","freiJoao59","norton_fatecig");
 			$codigo = $_POST["codigo"];
-			$sql = "select * from usuario where codigo=$codigo";
+			$sql = "select * from conteudo where codigo=$codigo";
 			$registros = mysqli_query($con,$sql);
 			if($campos = mysqli_fetch_array($registros)){
 				echo "<script lang='javascript'>";
-				echo "codigo.value='".$campos["codigo"] ."';";
 				echo "nome.value='".  $campos["nome"] ."';";
 				echo "titulo.value='". $campos["titulo"] ."';";
 				echo "conteudo.value='". $campos["conteudo"] ."';";
@@ -130,7 +162,7 @@
 	{
 			$con = new mysqli("baratheon0001.hospedagemdesites.ws","norto_fatecig","freiJoao59","norton_fatecig");
 			$codigo = $_POST["codigo"];
-			$sql = "delete from usuario where codigo=$codigo";
+			$sql = "delete from conteudo where codigo=$codigo";
 			mysqli_query($con,$sql);
 			echo "registro removido	com sucesso";
 			$con->close();	
@@ -138,16 +170,15 @@
 	function inserir()
 	{ 
 			$con = new mysqli("baratheon0001.hospedagemdesites.ws","norto_fatecig","freiJoao59","norton_fatecig");
-			$codigo = $_POST["codigo"];
+			$tipo	= $_POST["tipo"];
 			$nome	= $_POST["nome"];
 			$titulo	= $_POST["titulo"];
 			$conteudo = $_POST["conteudo"];	
 			$status = $_POST["status"];
 			$keywords = $_POST["keywords"];
-			$tipo = $_POST["tipo"];
-			$data_publicado = $_POST["data_publicado"];	
-			$sql = "insert into conteudo(codigo,nome, titulo, conteudo, status, keywords, tipo, data_publicado) 
-			values($codigo, '$nome', '$titulo', '$conteudo', '$status', '$keywords', '$tipo', $data_publicado)";
+			$tipo = $_POST["tipo"];	
+			$sql = "insert into conteudo(nome, titulo, conteudo, status, keywords, tipo, data_publicado) 
+			values('$nome', '$titulo', '$conteudo', '$status', '$keywords', '$tipo', NOW())";
 			mysqli_query($con,$sql);
 			echo "registro inserido com sucesso !";
 			$con->close();
@@ -161,9 +192,8 @@
 			$status = $_POST["status"];
 			$keywords = $_POST["keywords"];
 			$tipo = $_POST["tipo"];
-			$data_publicado = $_POST["data_publicado"];	
-			$sql = "update usuario set nome='$nome', titulo='$titulo',conteudo='$conteudo',status='$status',
-			keywords='$keywords',tipo='$tipo', data_publicado=$data_publicado where codigo=$codigo";
+			$sql = "update conteudo set nome='$nome', titulo='$titulo',conteudo='$conteudo',status='$status',
+			keywords='$keywords',tipo='$tipo' where codigo=$codigo";
 			mysqli_query($con,$sql);
 			echo "registro alterado com sucesso !";
 			$con->close();
